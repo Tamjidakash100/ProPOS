ebcb28fa31d69123aec5d59264781c92e63ccbe4
Rajdip27 <srajdip920@gmail.com>
2023-12-06 17:54:00 +0600

    Add Country

---
+++ src/App/ProPOS.App/Controllers/Base/ApiControllerBase.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProPOS.SharedKernel.Extensions.Results;
+
+namespace ProPOS.App.Controllers.Base;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApiControllerBase : ControllerBase
+{
+    private ISender _sender;
+    protected ISender Mediator => _sender ??= HttpContext.RequestServices.GetService<ISender>();
+
+    protected async Task<ActionResult> HandelCommandAsync<T>(IRequest<CommandResult<T>> command)
+    {
+        var result = await Mediator.Send(command);
+        return result.Type switch
+        {
+            CommandResultTypeEnum.BadRequest => new BadRequestResult(),
+            CommandResultTypeEnum.NotFound => new NotFoundResult(),
+            CommandResultTypeEnum.Conflict => new ConflictResult(),
+            CommandResultTypeEnum.Unauthorized => new UnauthorizedResult(),
+            CommandResultTypeEnum.Created => new CreatedResult("", result.Result),
+            _ => new OkObjectResult(result.Result)
+        };
+    }
+
+    protected async Task<ActionResult> HandelQueryAsync<T>(IRequest<QueryResult<T>> query)
+    {
+        var result = await Mediator.Send(query);
+        return result.Type switch
+        {
+            QueryResultTypeEnum.InvalidInput => new BadRequestResult(),
+            QueryResultTypeEnum.NotFound => new NotFoundResult(),
+            _ => new OkObjectResult(result.Result)
+        };
+    }
+}
---
+++ src/App/ProPOS.App/Controllers/CountryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProPOS.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+    }
+}
